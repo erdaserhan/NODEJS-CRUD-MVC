7fd29a8ea24f1f0cd9d7a4ed5a100cd7305e988e
erdaserhan <erhan209@gmail.com>
2024-10-02 13:56:41 +0200

    view updated

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -14,6 +14,53 @@ exports.create = (req, res) => {
         console.log('Le contenu ne peut pas être vide');
         res.redirect('contact_form');
     } else {
-        console.log(req.body)
+        console.log(req.body);
+
+        const unMsg = new Message({
+            nom: req.body.nom,
+            msg: req.body.nom
+        });
+
+        Message.create(unMsg, function(err, data) {
+            if(err) {
+                res.status(500).send({
+                    message: "Erreur pendant la création du message"
+                });
+            }else {
+                console.log('Data: ', data);
+                res.render('traiter_form', { titre: titrePage, nom: unMsg.nom, msg: unMsg.msg});
+            }
+        });
     }
+};
+
+exports.readAll = (req, res) => {
+    console.log('GET Tous les messages');
+    Message.readAll(function(err, data){
+        if(err) {
+            res.status(500).send({
+                message: "Erreur pendant la création du message"
+            });
+        } else {
+            console.log('Data :', data);
+            const titrePage = "Tableau des messages";
+            moment.locale('fr');
+            res.render('listeMessages', {titre: titrePage, donnees: data, moment: moment});
+        }
+    });
+};
+
+exports.newmsg = (req, res) => {
+    console.log("Affichage du formulaire de création")
+}; 
+
+exports.list = (req, res) => {
+    console.log('Affichage de la liste des messages');
+    Message.readAll(function(err, data) {
+        if(err) {
+            res.status(500).send({
+                message: ""
+            })
+        }
+    })
 };
