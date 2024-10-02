077a03b232f62c9a0b5ac328a1f68630057eb34c
erdaserhan <erhan209@gmail.com>
2024-10-02 16:10:49 +0200

    update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var messagesRouter = require('./routes/messages');
+const { title } = require('process');
 
 var app = express();
 
@@ -21,12 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
-app.use('/message', messagesRouter);
+app.use('/messages', messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -36,7 +37,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title: 'Erreur'});
 });
 
 module.exports = app;
